ccc6e57c2f30f016045748e6755c4231236f3f6a
yehudamarc <yehudamarc@gmail.com>
2020-04-27 19:42:40 +0300

    sigtest user program

---
+++ sigtest.c
@@ -0,0 +1,22 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+	/*
+	uint old_mask;
+	uint new_mask;
+	uint mask = 2020;
+	printf(1, "%d\n", mask);
+
+  	old_mask = sigprocmask(mask);
+  	printf(1, "%d\n", old_mask);
+
+  	new_mask = getpid();
+  	printf(1, "%d\n", new_mask);
+	*/
+	
+  exit();
+}
