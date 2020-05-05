946ee32ad57dd72d16f9e687ffae71b6a62a6709
yehudamarc <yehudamarc@gmail.com>
2020-05-05 16:28:32 +0300

    changes allocproc

--- proc.c
+++ proc.c
@@ -76,7 +76,7 @@ allocpid(void)
     pid = nextpid;
   } while(!cas(&nextpid, pid, pid+1));
   return pid+1;
-  
+
   // acquire(&ptable.lock);
   // pid = nextpid++;
   // release(&ptable.lock);
@@ -94,18 +94,18 @@ allocproc(void)
   struct proc *p;
   char *sp;
 
-  acquire(&ptable.lock);
+  // acquire(&ptable.lock);
 
   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    if(p->state == UNUSED)
+    if(cas(&p->state, UNUSED, EMBRYO))
       goto found;
 
-  release(&ptable.lock);
+  // release(&ptable.lock);
   return 0;
 
 found:
-  p->state = EMBRYO;
-  release(&ptable.lock);
+  // p->state = EMBRYO;
+  // release(&ptable.lock);
 
   p->pid = allocpid();
 
